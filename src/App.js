c1b3ffc6ff7f8293194d04023b04711aade932e9
uglyprincess <the.boy.who.lived.3x4@gmail.com>
2021-04-03 18:33:32 +0530

    Added Upload Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import UploadImage from './components/UploadImage';
 import ViewImages from './components/ViewImages';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
   return (
-
+    <Router>
+      <Switch>
+        <Route path="/upload"><UploadImage/></Route>
+        <Route path="/view"><ViewImages/></Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -2,21 +2,46 @@ import {React, useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import axios from 'axios';
 import querystring from 'querystring';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+    form: {
+        '& > *': {
+          width: '25ch',
+          display: 'flex',
+          flexDirection: 'column',
+          margin: '50px auto'
+        },
+    },
     body: {
-
+        backgroundImage: 'url(https://www.imaginefactory.com/wp-content/uploads/2017/04/SPACES_BG-2.jpg)',
+        backgroundSize: 'cover',
+        height:'100vh',
+        maxWidth: '100%',
+        overflow: 'hidden',
+        objectFit: 'fill',
     },
 
     root: {
-
+        border: '2px solid black',
+        width: '35%',
+        margin: '50px auto 80px auto',
+        paddingBottom: '50px',
+        borderRadius: '10%',
+        backgroundColor: 'white',
     },
 
     heading: {
-
+        textAlign: 'center',
+        paddingTop: '50px',
     },
 
-});
+    input: {
+        display: 'none',
+    }
+
+}));
 
 export default function UploadImage() {
 
@@ -24,34 +49,66 @@ export default function UploadImage() {
     
     const [name, changeName] = useState("");
     const [desc, changeDesc] = useState("");
+    const [img, updateImg] = useState("");
     
     function sendInfo() {
-        axios.post("http://localhost:4000/api/upload", querystring({}), {
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-            }
-        })
-        .then(response => {
+            
+        const formData = new FormData();
+        
+        formData.append("name", name);
+        formData.append("desc", desc);
+        formData.append("image", img);
+
+        axios.post("http://localhost:4000/api/upload", formData).then(response => {
             if(response.status === 200) {
                 console.log("Image uploaded!");
             }
+        })
+        .catch(error => {
+            console.log(error);
         });
     };
+
+    function handleName(event) {
+        changeName(event.target.value);
+    }
+
+    function handleDesc(event) {
+        changeDesc(event.target.value);
+    }
+
+    function handleImg(event) {
+        updateImg(event.target.files[0]);
+        console.log(img);
+    }
 
     return (<div className={classes.body}>
         <div className={classes.root}>
             <div className={classes.heading}>
                 <h1>Upload Image</h1>
             </div>
-            <div>
+            <form className={classes.form} noValidate autoComplete="off" encType="multipart/form-data">
+                <TextField id="outlined-basic" label="Name" variant="outlined" value={name} onChange={handleName}/>
+                <TextField id="outlined-basic" label="Description" variant="outlined" value={desc} onChange={handleDesc}/>
+                <input
+                    accept="image/*"
+                    className={classes.input}
+                    id="contained-button-file"
+                    multiple
+                    type="file"
+                    filename="image"
+                    onChange={handleImg}
+                />
+                <label htmlFor="contained-button-file">
+                    <Button variant="contained" color="primary" component="span">
+                        Choose file
+                    </Button>
+                </label>
+                <Button variant="contained" color="primary" onClick={sendInfo}>
+                    Upload
+                </Button>
+            </form>
 
-            </div>
-            <div>
-
-            </div>
-            <div>
-
-            </div>
         </div>
     </div>);
 }
